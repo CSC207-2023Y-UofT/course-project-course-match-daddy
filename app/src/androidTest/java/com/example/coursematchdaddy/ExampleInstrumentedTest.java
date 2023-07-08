a11df11236cb659ce1f515f18cdaf2960c3d652c
realdevvora <legitvoradev@gmail.com>
2023-07-07 20:15:53 -0400

    Test

--- app/src/androidTest/java/com/example/coursematchdaddy/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/coursematchdaddy/ExampleInstrumentedTest.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.*;
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * /////
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/Course.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/Course.java
@@ -1,4 +1,12 @@
 package com.example.coursematchdaddy.clean_architecture_layers.entities.classes;
 
 public class Course {
+    public Course() {
+
+    }
+
+    public void hi() {
+        int i = 1;
+    }
+
 }
