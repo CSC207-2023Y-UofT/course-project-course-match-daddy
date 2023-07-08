cfe3e460da140c94a1f114bf2c3bb83a60bf1153
uyoyo0 <uyiosa.iyekekpolor@mail.utoronto.ca>
2023-07-08 18:15:24 -0400

    added the methods and attributes for methods im responsible for

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/login_class_imports_implementations/CollectLoginDataInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/login_class_imports_implementations/CollectLoginDataInterface.java
@@ -1,4 +1,9 @@
 package com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.login_class_imports_implementations;
 
 public interface CollectLoginDataInterface {
+
+     void storeUsername(String username);
+     void storePassword(String password);
+     void storeEmail(String email);
+     
 }
