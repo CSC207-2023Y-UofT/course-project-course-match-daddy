6bb890022fbe13a7b6ac68929f0c7e402b6a1383
realdevvora <legitvoradev@gmail.com>
2023-07-10 12:37:44 -0400

    Create Program entity

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/Program.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/Program.java
@@ -1,4 +1,24 @@
 package com.example.coursematchdaddy.clean_architecture_layers.entities.classes;
 
 public class Program {
+    private String programTitle;
+    private String programCode;
+    private String programDescription;
+
+    public Program(String programTitle, String programCode, String programDescription) {
+        this.programTitle = programTitle;
+        this.programCode = programCode;
+        this.programDescription = programDescription;
+    }
+
+    public String getProgramTitle() {
+        return this.programTitle;
+    }
+    public String getProgramCode() {
+        return this.programCode;
+    }
+    public String getProgramDescription() {
+        return this.programDescription;
+    }
+
 }
