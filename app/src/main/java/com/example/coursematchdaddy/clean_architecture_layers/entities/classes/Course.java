8b90c173810349860f040daa8a6a7a5877c967df
realdevvora <legitvoradev@gmail.com>
2023-07-10 12:13:03 -0400

    Create Course entity

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/Course.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/Course.java
@@ -1,12 +1,38 @@
 package com.example.coursematchdaddy.clean_architecture_layers.entities.classes;
 
+import java.util.HashMap;
+
 public class Course {
-    public Course() {
-
+    private String courseTitle;
+    private String courseCode;
+    private String courseDescription;
+    private String courseImageURL;
+    private HashMap<String, Object> miscellaneousCourseData;
+    public Course(String courseTitle, String courseCode,
+                  String courseDescription, String courseImageURL,
+                  HashMap<String, Object> miscellaneousCourseData) {
+        this.courseTitle = courseTitle;
+        this.courseCode = courseCode;
+        this.courseDescription = courseDescription;
+        this.courseImageURL = courseImageURL;
+        this.miscellaneousCourseData = miscellaneousCourseData;
     }
 
-    public void hi() {
-        int i = 1;
+    public String getCourseTitle() {
+        return this.courseTitle;
     }
+    public String getCourseCode() {
+        return this.courseCode;
+    }
+    public String getCourseDescription() {
+        return this.courseDescription;
+    }
+    public String getCourseImageURL() {
+        return this.courseImageURL;
+    }
+    public HashMap<String, Object> getMiscellaneousCourseData() {
+        return this.miscellaneousCourseData;
+    }
+
 
 }
