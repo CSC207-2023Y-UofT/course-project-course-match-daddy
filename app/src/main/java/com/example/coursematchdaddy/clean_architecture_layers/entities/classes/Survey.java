c1c2b05aebf196b6981f4bf263fa9d757914a479
GagandeepLubana <gagandeeplubana1221@gmail.com>
2023-07-12 13:18:35 -0400

    Completed Survey Entity class

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/Survey.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/Survey.java
@@ -1,4 +1,33 @@
 package com.example.coursematchdaddy.clean_architecture_layers.entities.classes;
 
-public class Survey {
+import java.util.ArrayList;
+
+public abstract class Survey {
+    private String program;
+    private Integer numCredits;
+    private ArrayList<String> courses;
+    private ArrayList<String> preferences;
+
+    public Survey(String program, Integer numCredits, ArrayList<String> courses, ArrayList<String> preferences) {
+        this.program = program;
+        this.numCredits = numCredits;
+        this.courses = courses;
+        this.preferences = preferences;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public Integer getNumCredits() {
+        return numCredits;
+    }
+
+    public ArrayList<String> getCourses() {
+        return courses;
+    }
+
+    public ArrayList<String> getPreferences() {
+        return preferences;
+    }
 }
--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/interfaces/survey_class_imports_implementations/UserDataInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/interfaces/survey_class_imports_implementations/UserDataInterface.java
@@ -3,11 +3,4 @@ package com.example.coursematchdaddy.clean_architecture_layers.entities.interfac
 import java.util.ArrayList;
 
 public interface UserDataInterface {
-    public void setProgram(String program);
-
-    public void setCredits(int num);
-
-    public void setCourses(ArrayList<String> courses);
-
-    public void setPreferences(ArrayList<String> preferences);
 }
