bbe3b26d235785916dbfb8eb69347966a0c3a874
realdevvora <legitvoradev@gmail.com>
2023-07-13 19:36:07 -0400

    Implement enroll method and interface

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/interfaces/course_class_imports_implementations/ArtsAndSciencesCourseInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/interfaces/course_class_imports_implementations/ArtsAndSciencesCourseInterface.java
@@ -1,4 +1,16 @@
 package com.example.coursematchdaddy.clean_architecture_layers.entities.interfaces.course_class_imports_implementations;
 
+import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Course;
+import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
+
 public interface ArtsAndSciencesCourseInterface {
+
+    public String getProgramArea();
+    public String getBreadth();
+    public int getRemainingSeats();
+    public String getDistribution();
+    public boolean enroll(User user);
+
+    public boolean unenroll(User user);
+
 }
