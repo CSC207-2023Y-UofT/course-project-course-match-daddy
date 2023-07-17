2d3139d4872600aac38d2bf9313a82e10dd320d9
GagandeepLubana <gagandeeplubana1221@gmail.com>
2023-07-17 13:33:25 -0400

    created UserDataInterface and subclass

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/survey_subclasses/UserData.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/survey_subclasses/UserData.java
@@ -1,6 +1,28 @@
 package com.example.coursematchdaddy.clean_architecture_layers.entities.classes.survey_subclasses;
 
 import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Survey;
+import com.example.coursematchdaddy.clean_architecture_layers.entities.interfaces.survey_class_imports_implementations.UserDataInterface;
 
-public class UserData extends Survey {
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class UserData extends Survey implements UserDataInterface {
+
+    public UserData(String program, Integer numCredits, ArrayList<String> courses, ArrayList<String> preferences) {
+        super(program, numCredits, courses, preferences);
+
+
+    }
+
+    public HashMap<String, Object> getUserSurveyData() {
+        HashMap<String, Object> data = new HashMap<>();
+
+        data.put("program", getProgram());
+        data.put("numOfCredits", getNumCredits());
+        data.put("courses", getCourses());
+        data.put("preferences", getPreferences());
+        return data;
+    }
+
+    
 }
--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/interfaces/survey_class_imports_implementations/UserDataInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/interfaces/survey_class_imports_implementations/UserDataInterface.java
@@ -1,4 +1,8 @@
 package com.example.coursematchdaddy.clean_architecture_layers.entities.interfaces.survey_class_imports_implementations;
 
+import java.util.HashMap;
+
 public interface UserDataInterface {
+    HashMap<String, Object> getUserSurveyData();
+
 }
