cc7352ffef97a95d0a838183f8394cc82709b622
realdevvora <legitvoradev@gmail.com>
2023-07-17 19:44:06 -0400

    Add Type3 program info

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/program_subclasses/Type3Program.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/program_subclasses/Type3Program.java
@@ -1,6 +1,20 @@
 package com.example.coursematchdaddy.clean_architecture_layers.entities.classes.program_subclasses;
 
 import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Program;
+import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
 
 public class Type3Program extends Program {
+    public boolean enroll(User userData, boolean hasRequirements, boolean hasSpace) {
+        if (hasRequirements && hasSpace) {
+            userData.getSelectedPrograms().put(getProgramTitle(), this);
+            return true;
+        }
+        return false;
+    }
+    public boolean unenroll(User userData) {
+        if (userData.getSelectedPrograms().remove(getProgramTitle())) {
+            return true;
+        }
+        return false;
+    }
 }
