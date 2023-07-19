e3b064283110aceecc9397c791f0c5d61b918823
realdevvora <legitvoradev@gmail.com>
2023-07-19 17:16:54 -0400

    Add docstrings, minor adjustments

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/course_subclasses/ArtsAndSciencesCourse.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/course_subclasses/ArtsAndSciencesCourse.java
@@ -4,14 +4,29 @@ import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.C
 import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
 
 public class ArtsAndSciencesCourse extends Course {
+
+    /**
+     * enroll a User into this course if there are seats available
+     * @param userData
+     * @return
+     */
     public boolean enroll(User userData) {
         if (this.getRemainingSeats() > 0) {
             // enroll user
             userData.getSelectedCourses().put(this.getCourseTitle(), this);
             // subtract from remaining seats
-            this.getMiscellaneousCourseData.put("RemainingSeats", this.getMiscellaneousCourseData.
-                    get("RemainingSeats") - 1);
+            updateSeats(-1);
         }
         return false;
+    }
+
+    /**
+     * update the number of seats in this course by amount
+     * @param amount
+     */
+    private void updateSeats(int amount) {
+        // update the seats by adding in the amount to the RemainingSeats key-pair in misc data
+        this.getMiscellaneousCourseData.put("RemainingSeats", this.getMiscellaneousCourseData.
+                get("RemainingSeats") + amount);
     }
 }
