551303c35f2e22b28a8e9f57ccf2a5011c629a86
realdevvora <legitvoradev@gmail.com>
2023-07-19 17:39:14 -0400

    Add docstrings

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/program_subclasses/Type2Program.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/program_subclasses/Type2Program.java
@@ -5,13 +5,26 @@ import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.U
 
 public class Type2Program extends Program {
 
+    /**
+     * Enroll userData in this program only if they have fulfilled the requirements
+     * @param userData
+     * @param hasRequirements
+     * @return
+     */
     public boolean enroll(User userData, boolean hasRequirements) {
         if (hasRequirements) {
+            // add program to user's selected programs
             userData.getSelectedPrograms().put(getProgramTitle(), this);
             return true;
         }
         return false;
     }
+
+    /**
+     * Unenroll userData
+     * @param userData
+     * @return
+     */
     public boolean unenroll(User userData) {
         if (userData.getSelectedPrograms().remove(getProgramTitle())) {
             return true;
