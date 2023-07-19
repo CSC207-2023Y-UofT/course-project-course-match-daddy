678ebb953da5ad3a45a0470bc686cdcc9d03949d
realdevvora <legitvoradev@gmail.com>
2023-07-19 16:58:59 -0400

    Added docstrings

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/program_subclasses/Type1Program.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/program_subclasses/Type1Program.java
@@ -4,10 +4,22 @@ import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.P
 import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
 
 public class Type1Program extends Program {
+
+    /**
+     * Add this user to their selected programs
+     * @param userData
+     * @return
+     */
     public boolean enroll(User userData) {
         userData.getSelectedPrograms().put(getProgramTitle(), this);
         return true;
     }
+
+    /**
+     * Remove this user from their selected programs
+     * @param userData
+     * @return
+     */
     public boolean unenroll(User userData) {
         if (userData.getSelectedPrograms().remove(getProgramTitle())) {
             return true;
