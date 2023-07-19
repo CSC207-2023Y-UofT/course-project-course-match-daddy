00ab459f8a0938f4c211cb730a3f02f7545638c7
realdevvora <legitvoradev@gmail.com>
2023-07-19 17:08:17 -0400

    Add docstrings

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/course_subclasses/ClosedEnrollmentCourse.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/course_subclasses/ClosedEnrollmentCourse.java
@@ -4,10 +4,22 @@ import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.C
 import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
 
 public class ClosedEnrollmentCourse extends Course {
+    /**
+     * Check if user has the prerequisite courses completed for this course
+     * @param user
+     * @return
+     */
     public boolean hasPrerequisites(User user) {
         // placeholder method to show if prerequisites exist or not (will almost always return true)
         return this.getMiscellaneousCourseData.get("PrerequisitesMet") != "";
     }
+
+    /**
+     * Enroll user into this course if they have met the prerequisites and the course has seats
+     * available
+     * @param userData
+     * @return
+     */
     public boolean enroll(User userData) {
         // if user has prerequisites and seats are available
         if (hasPrerequisites(userData) && getRemainingSeats() > 0) {
@@ -19,6 +31,11 @@ public class ClosedEnrollmentCourse extends Course {
         }
         return false;
     }
+
+    /**
+     * Update the course's seats by amount
+     * @param amount
+     */
     private void updateSeats(int amount) {
         // update the seats by adding in the amount to the RemainingSeats key-pair in misc data
         this.getMiscellaneousCourseData.put("RemainingSeats", this.getMiscellaneousCourseData.
