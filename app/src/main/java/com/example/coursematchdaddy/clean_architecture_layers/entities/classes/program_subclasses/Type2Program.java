d358b47299758fc5044a294ae540c48c93a58dc4
realdevvora <legitvoradev@gmail.com>
2023-07-18 20:16:50 -0400

    Added User import

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/program_subclasses/Type2Program.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/program_subclasses/Type2Program.java
@@ -1,6 +1,7 @@
 package com.example.coursematchdaddy.clean_architecture_layers.entities.classes.program_subclasses;
 
 import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Program;
+import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
 
 public class Type2Program extends Program {
 
