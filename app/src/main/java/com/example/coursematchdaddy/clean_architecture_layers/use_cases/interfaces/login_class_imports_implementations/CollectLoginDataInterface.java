e1364d4ed28df39e3d2c82c37a368ba515d1997c
uyoyo0 <96444432+uyoyo0@users.noreply.github.com>
2023-07-20 09:34:03 -0400

    wrong branch

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/login_class_imports_implementations/CollectLoginDataInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.login_class_imports_implementations;
-
-public interface CollectLoginDataInterface {
-
-     void storeUsername(String username);
-     void storePassword(String password);
-     void storeEmail(String email);
-     
-}
