c3cbce2634d13edb79dec67e9fb7548968d7ae63
realdevvora <91434020+realdevvora@users.noreply.github.com>
2023-07-20 19:11:30 -0400

    Update OpenEnrollmentCourse.java

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/course_subclasses/OpenEnrollmentCourse.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/course_subclasses/OpenEnrollmentCourse.java
@@ -1,7 +1,6 @@
 package com.example.coursematchdaddy.clean_architecture_layers.entities.classes.course_subclasses;
 
 import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Course;
-import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
 
 public class OpenEnrollmentCourse extends Course {
 }
