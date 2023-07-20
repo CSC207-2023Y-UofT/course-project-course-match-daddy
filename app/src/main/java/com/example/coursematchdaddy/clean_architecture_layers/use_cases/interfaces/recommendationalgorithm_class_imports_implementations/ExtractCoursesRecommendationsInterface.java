4c3ba53789d479e90809aac52452e1c09df1afe1
realdevvora <legitvoradev@gmail.com>
2023-07-20 09:20:46 -0400

    Moved to correct interface

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/recommendationalgorithm_class_imports_implementations/ExtractCoursesRecommendationsInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/recommendationalgorithm_class_imports_implementations/ExtractCoursesRecommendationsInterface.java
@@ -1,13 +1,4 @@
 package com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.recommendationalgorithm_class_imports_implementations;
 
-import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Course;
-
-import java.util.HashMap;
-
 public interface ExtractCoursesRecommendationsInterface {
-    public String getCourseTitle(Course courseData);
-    public String getCourseData();
-    public String getCourseDescription(Course courseData);
-    public String getImageURL(Course courseData);
-    public HashMap<String, Object> getMiscellaneousCourseData(Course courseData);
 }
