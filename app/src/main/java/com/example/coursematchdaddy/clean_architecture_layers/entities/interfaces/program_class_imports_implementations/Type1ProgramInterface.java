c39758503f0a4683f9f15f8c5a98433ea45ffe21
realdevvora <91434020+realdevvora@users.noreply.github.com>
2023-07-20 18:52:22 -0400

    Update Type1ProgramInterface.java

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/interfaces/program_class_imports_implementations/Type1ProgramInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/interfaces/program_class_imports_implementations/Type1ProgramInterface.java
@@ -1,8 +1,4 @@
 package com.example.coursematchdaddy.clean_architecture_layers.entities.interfaces.program_class_imports_implementations;
 
-import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
-
 public interface Type1ProgramInterface {
-    public boolean enroll(User userData);
-    public boolean unenroll(User userData);
 }
