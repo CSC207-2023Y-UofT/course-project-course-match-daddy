0d843dd8e619648033237174569949bd542be59a
uyoyo0 <96444432+uyoyo0@users.noreply.github.com>
2023-07-20 09:24:02 -0400

    added comments to DBUsersGateway.java

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/gateways/classes/DBUsersGateway.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/gateways/classes/DBUsersGateway.java
@@ -11,34 +11,57 @@ import com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfac
 import com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.updatesettings_class_imports_implementations.SaveSurveyDataInterface;
 import com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.updatesettings_class_imports_implementations.UpdateSettingsDataInterface;
 
-public class DBUsersGateway implements CreateUserAccountInterface, VerifyLoginDataInterface, SwipeCardRightInterface, SwipeCardLeftInterface, ExtractUserDataInterface, ExtractCoursesRecommendationsInterface, ExtractProgramsRecommendationsInterface, SaveSurveyDataInterface, UpdateSettingsDataInterface {
-    public boolean verifyUsername(User userData){
-        /**
-        I'm assuming that the <getUsername> method will be implemented here as per the design doc
-        Also note, <username> is listed as a public attribute of User on the design doc which
-        Is why im not using a getter for it. I also assume that the "get" methods in this class
-         will somehow access whatever persistence we choose to use to fetch the needed data. Please
-         correct me if I am misunderstanding anything
-         **/
-        if (userData.username.equals(getUsername(userData))){
+public class DBUsersGateway implements CreateUserAccountInterface, VerifyLoginDataInterface, SwipeCardRightInterface,
+        SwipeCardLeftInterface, ExtractUserDataInterface, ExtractCoursesRecommendationsInterface,
+        ExtractProgramsRecommendationsInterface, SaveSurveyDataInterface, UpdateSettingsDataInterface {
 
+  /**
+  IMPORTANT:
+    I'm assuming that the <getUsername> method will be implemented here as per the design doc
+    Also note, <username> is listed as a public attribute of User on the design doc which
+    Is why im not using a getter for it. I also assume that the "get" methods in this class
+     will somehow access whatever persistence we choose to use to fetch the needed data. Please
+     correct me if I am misunderstanding anything
+     **/
+    
+    /**
+     * Verifies the correctness of the provided username in the user data.
+     *
+     * @param userData The User object containing the user data, including the username to be verified.
+     * @return true if the username is valid and matches an existing user account, false otherwise.
+     */
+    public boolean verifyUsername(User userData){
+        if (userData.username.equals(getUsername(userData))){
             return true;
-        }else{
+        } else {
             return false;
         }
     }
+
+    /**
+     * Verifies the correctness of the provided password in the user data.
+     *
+     * @param userData The User object containing the user data, including the password to be verified.
+     * @return true if the password is valid and matches the corresponding user's password, false otherwise.
+     */
     public boolean verifyPassword(User userData){
         if (userData.getPassword().equals(getPassword(userData))){
             return true;
-        }else{
+        } else {
             return false;
         }
-        return false;
     }
+
+    /**
+     * Verifies the correctness of the provided email in the user data.
+     *
+     * @param userData The User object containing the user data, including the email to be verified.
+     * @return true if the email is valid and matches an existing user account, false otherwise.
+     */
     public boolean verifyEmail(User userData){
         if (userData.getEmail().equals(getEmail(userData))){
             return true;
-        }else{
+        } else {
             return false;
         }
     }
