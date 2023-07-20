9209beeb0163dcf0b193ffb54cedab1247a1fc53
uyoyo0 <96444432+uyoyo0@users.noreply.github.com>
2023-07-20 22:50:40 +0000

    Revert "added the methods and attributes for methods im responsible for"
    This reverts commit cfe3e460da140c94a1f114bf2c3bb83a60bf1153.

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/interfaces/user_class_imports_implementations/LoggedInUserInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/interfaces/user_class_imports_implementations/LoggedInUserInterface.java
@@ -1,9 +1,4 @@
 package com.example.coursematchdaddy.clean_architecture_layers.entities.interfaces.user_class_imports_implementations;
 
-import java.util.HashMap;
-
 public interface LoggedInUserInterface {
-    HashMap<String, Object> getLoginCredentials();
-    HashMap<String, Object> getUserData();
-
 }
