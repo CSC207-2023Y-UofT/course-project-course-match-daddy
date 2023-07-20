3a110cfa93a3916a5373cd66e7a55f872abd768d
uyoyo0 <96444432+uyoyo0@users.noreply.github.com>
2023-07-20 19:24:53 -0400

    Update CreateUserAccountInterface.java

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/login_class_imports_implementations/CreateUserAccountInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/login_class_imports_implementations/CreateUserAccountInterface.java
@@ -1,13 +1,4 @@
 package com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.login_class_imports_implementations;
 
-import com.example.coursematchdaddy.clean_architecture_layers.gateways.classes.DBUsersGateway;
-
 public interface CreateUserAccountInterface {
-
-    final static DBUsersGateway gateway = new DBUsersGateway();
-    
-    boolean addUsername(String username);
-    boolean addPassword(String password);
-    boolean addEmail(String email);
-
 }
