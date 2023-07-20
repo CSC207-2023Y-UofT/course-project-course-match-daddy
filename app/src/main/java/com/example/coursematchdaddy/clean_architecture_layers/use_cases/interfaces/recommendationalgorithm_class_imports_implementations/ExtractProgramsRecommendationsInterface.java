f8987a13e45fac187418cf8f520c14e6cc407564
realdevvora <legitvoradev@gmail.com>
2023-07-20 09:22:44 -0400

    Moved to correct interface

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/recommendationalgorithm_class_imports_implementations/ExtractProgramsRecommendationsInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/recommendationalgorithm_class_imports_implementations/ExtractProgramsRecommendationsInterface.java
@@ -1,33 +1,4 @@
 package com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.recommendationalgorithm_class_imports_implementations;
 
-import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Program;
-
 public interface ExtractProgramsRecommendationsInterface {
-    /**
-     * return program title from the database
-     * @param programData: Program
-     * @return String
-     */
-    public String getTitle(Program programData);
-
-    /**
-     * return program code from the database
-     * @param programData: Program
-     * @return String
-     */
-    public String getCode(Program programData);
-
-    /**
-     * return program description from the database
-     * @param programData: Program
-     * @return String
-     */
-    public String getDescription(Program programData);
-
-    /**
-     * return program image url from the database
-     * @param programData: Program
-     * @return String
-     */
-    public String getImageURL(Program programData);
 }
--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/swipecarousel_class_imports_implementations/ExtractProgramDataInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/swipecarousel_class_imports_implementations/ExtractProgramDataInterface.java
@@ -1,4 +1,34 @@
 package com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.swipecarousel_class_imports_implementations;
 
+import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Program;
+
 public interface ExtractProgramDataInterface {
+    
+    /**
+     * return program title from the database
+     * @param programData: Program
+     * @return String
+     */
+    public String getTitle(Program programData);
+
+    /**
+     * return program code from the database
+     * @param programData: Program
+     * @return String
+     */
+    public String getCode(Program programData);
+
+    /**
+     * return program description from the database
+     * @param programData: Program
+     * @return String
+     */
+    public String getDescription(Program programData);
+
+    /**
+     * return program image url from the database
+     * @param programData: Program
+     * @return String
+     */
+    public String getImageURL(Program programData);
 }
