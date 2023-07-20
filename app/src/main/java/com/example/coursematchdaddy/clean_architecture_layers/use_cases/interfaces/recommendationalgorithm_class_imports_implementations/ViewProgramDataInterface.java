fde6978eef62e7a8821ae94506fa092cbf22466f
realdevvora <legitvoradev@gmail.com>
2023-07-19 21:45:55 -0400

    Add method to interface

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/recommendationalgorithm_class_imports_implementations/ViewProgramDataInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/recommendationalgorithm_class_imports_implementations/ViewProgramDataInterface.java
@@ -1,4 +1,7 @@
 package com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.recommendationalgorithm_class_imports_implementations;
 
+import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Program;
+
 public interface ViewProgramDataInterface {
+    public Program getProgramData(String programCode);
 }
