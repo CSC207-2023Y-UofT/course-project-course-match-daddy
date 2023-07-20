4a9e1332293b86e7b3bdde252f19b2ec4e1aa986
realdevvora <91434020+realdevvora@users.noreply.github.com>
2023-07-20 19:14:56 -0400

    Update OpenEnrollmentCourse.java

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/course_subclasses/OpenEnrollmentCourse.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/course_subclasses/OpenEnrollmentCourse.java
@@ -1,21 +1,6 @@
 package com.example.coursematchdaddy.clean_architecture_layers.entities.classes.course_subclasses;
 
 import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Course;
-import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
 
 public class OpenEnrollmentCourse extends Course {
-    public boolean enroll(User userData) {
-        if (this.getRemainingSeats() > 0) {
-            // enroll user
-            userData.getSelectedCourses().put(this.getCourseTitle(), this);
-            // subtract from remaining seats
-            updateSeats(-1);
-            return true;
-        }
-        return false;
-    }
-    private void updateSeats(int amount) {
-        this.getMiscellaneousCourseData.put("RemainingSeats", this.getMiscellaneousCourseData.
-                get("RemainingSeats") + amount);
-    }
 }
