ddf71a75f171884ae1dc39493b13be16d5ae6c58
uyoyo0 <96444432+uyoyo0@users.noreply.github.com>
2023-07-20 22:47:17 +0000

    Revert "added comments CreateUserAccountInterface.java"
    This reverts commit 9146f8639b0dd748d8eae4ac1d001eb11e67dd15.

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/login_class_imports_implementations/CreateUserAccountInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/login_class_imports_implementations/CreateUserAccountInterface.java
@@ -3,28 +3,11 @@ package com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfa
 import com.example.coursematchdaddy.clean_architecture_layers.gateways.classes.DBUsersGateway;
 
 public interface CreateUserAccountInterface {
-    /**
-     * Adds a username to the persistence.
-     *
-     * @param username The username to be added.
-     * @return true if the username is successfully added and is unique else return false.
-     */
-    public boolean addUsername(String username);
 
-    /**
-     * Adds a password to the persistence.
-     *
-     * @param password The password to be added.
-     * @return true if the password is successfully added and meets security requirements else return false.
-     */
-    public boolean addPassword(String password);
-
-    /**
-     * Adds email to persistence.
-     *
-     * @param email The email to be added.
-     * @return if the email is successfully added and is valid else return false.
-     */
-    public boolean addEmail(String email);
+    final static DBUsersGateway gateway = new DBUsersGateway();
+    
+    boolean addUsername(String username);
+    boolean addPassword(String password);
+    boolean addEmail(String email);
 
 }
