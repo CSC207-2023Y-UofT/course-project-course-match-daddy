6ddc053a2aaae067bdfbd6e07450b07bd2e6e2b4
realdevvora <91434020+realdevvora@users.noreply.github.com>
2023-07-20 19:47:11 -0400

    Update ViewCourseDataInterface.java

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/recommendationalgorithm_class_imports_implementations/ViewCourseDataInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/recommendationalgorithm_class_imports_implementations/ViewCourseDataInterface.java
@@ -1,7 +1,4 @@
 package com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.recommendationalgorithm_class_imports_implementations;
 
-import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Course;
-
 public interface ViewCourseDataInterface {
-    public Course getCourseData(String courseCode);
 }
