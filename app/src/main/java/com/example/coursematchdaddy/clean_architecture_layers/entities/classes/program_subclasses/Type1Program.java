b665092fe56f072f074a4c5e823f00981642c1ca
realdevvora <91434020+realdevvora@users.noreply.github.com>
2023-07-20 18:51:30 -0400

    Update Type1Program.java

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/program_subclasses/Type1Program.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/program_subclasses/Type1Program.java
@@ -3,13 +3,4 @@ package com.example.coursematchdaddy.clean_architecture_layers.entities.classes.
 import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Program;
 
 public class Type1Program extends Program {
-    public boolean enroll(User userData) {
-        userData.getSelectedPrograms().put(getProgramTitle(), this);
-        return true;
-    }
-    public boolean unenroll(User userData) {
-        userData.getSelectedPrograms().remove(getProgramTitle());
-        return true;
-    }
-
 }
