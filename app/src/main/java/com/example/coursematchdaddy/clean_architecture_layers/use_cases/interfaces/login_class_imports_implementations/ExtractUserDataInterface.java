050c5a3ee44610bbec5bdb6b013a3ff9a7fa94c0
realdevvora <legitvoradev@gmail.com>
2023-07-19 21:30:24 -0400

    Add methods to interface

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/login_class_imports_implementations/ExtractUserDataInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/login_class_imports_implementations/ExtractUserDataInterface.java
@@ -5,10 +5,4 @@ import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.C
 import java.util.HashMap;
 
 public interface ExtractUserDataInterface {
-    public String getCourseTitle(Course courseData);
-    public String getCourseData();
-    public String getCourseDescription(Course courseData);
-    public String getImageURL(Course courseData);
-    public HashMap<String, Object> getMiscellaneousCourseData(Course courseData);
-
 }
