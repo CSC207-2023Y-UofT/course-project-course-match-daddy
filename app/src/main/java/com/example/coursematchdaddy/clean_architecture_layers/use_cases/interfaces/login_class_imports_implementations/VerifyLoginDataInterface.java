d76fa15723a2db791dde70938665e66e3090fde3
uyoyo0 <96444432+uyoyo0@users.noreply.github.com>
2023-07-20 10:30:40 -0400

    added comments VerifyLoginDataInterface.java

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/login_class_imports_implementations/VerifyLoginDataInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/login_class_imports_implementations/VerifyLoginDataInterface.java
@@ -4,10 +4,29 @@ import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.U
 import com.example.coursematchdaddy.clean_architecture_layers.gateways.classes.DBUsersGateway;
 
 public interface VerifyLoginDataInterface {
-
-    final static DBUsersGateway gateway = new DBUsersGateway();
+    
+/**
+     * Verifies the correctness of the provided username in the user data.
+     *
+     * @param userData The User object containing the user data, including the username to be verified.
+     * @return true if the username is valid and matches an existing user account, false otherwise.
+     */
     boolean verifyUsername(User userData);
+
+    /**
+     * Verifies the correctness of the provided password in the user data.
+     *
+     * @param userData The User object containing the user data, including the password to be verified.
+     * @return true if the password is valid and matches the corresponding user's password, false otherwise.
+     */
     boolean verifyPassword(User userData);
+
+    /**
+     * Verifies the correctness of the provided email in the user data.
+     *
+     * @param userData The User object containing the user data, including the email to be verified.
+     * @return true if the email is valid and matches an existing user account, false otherwise.
+     */
     boolean verifyEmail(User userData);
 
 }
