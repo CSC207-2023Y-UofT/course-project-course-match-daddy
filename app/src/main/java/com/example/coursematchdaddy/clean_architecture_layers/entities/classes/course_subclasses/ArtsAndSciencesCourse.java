a341ccd854c7b2d5a32a4e53a9040f10a158bca9
realdevvora <91434020+realdevvora@users.noreply.github.com>
2023-07-20 18:54:30 -0400

    Update ArtsAndSciencesCourse.java

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/course_subclasses/ArtsAndSciencesCourse.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/course_subclasses/ArtsAndSciencesCourse.java
@@ -1,4 +1,6 @@
 package com.example.coursematchdaddy.clean_architecture_layers.entities.classes.course_subclasses;
 
+import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Course;
+
 public class ArtsAndSciencesCourse extends Course {
 }
