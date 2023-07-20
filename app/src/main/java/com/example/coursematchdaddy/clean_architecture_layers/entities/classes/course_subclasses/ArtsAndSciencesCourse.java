f5ab1e9313cf31f6bcc946374aa6dc266a5ce0a2
realdevvora <91434020+realdevvora@users.noreply.github.com>
2023-07-20 18:50:28 -0400

    Update ArtsAndSciencesCourse.java

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/course_subclasses/ArtsAndSciencesCourse.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/course_subclasses/ArtsAndSciencesCourse.java
@@ -1,17 +1,4 @@
 package com.example.coursematchdaddy.clean_architecture_layers.entities.classes.course_subclasses;
 
-import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Course;
-import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
-
 public class ArtsAndSciencesCourse extends Course {
-    public boolean enroll(User userData) {
-        if (this.getRemainingSeats() > 0) {
-            // enroll user
-            userData.getSelectedCourses().put(this.getCourseTitle(), this);
-            // subtract from remaining seats
-            this.getMiscellaneousCourseData.put("RemainingSeats", this.getMiscellaneousCourseData.
-                    get("RemainingSeats") - 1);
-        }
-        return false;
-    }
 }
