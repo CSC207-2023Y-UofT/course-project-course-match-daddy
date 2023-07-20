32a45bb2c3709036fe73c3a80fcc1efe9cf2e13c
uyoyo0 <96444432+uyoyo0@users.noreply.github.com>
2023-07-20 19:17:43 -0400

    Update LoginPresenter.java

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/presenters/classes/LoginPresenter.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/presenters/classes/LoginPresenter.java
@@ -1,6 +1,4 @@
 package com.example.coursematchdaddy.clean_architecture_layers.presenters.classes;
 
-import com.example.coursematchdaddy.clean_architecture_layers.controllers.classes.LoginController;
-
 public class LoginPresenter {
 }
