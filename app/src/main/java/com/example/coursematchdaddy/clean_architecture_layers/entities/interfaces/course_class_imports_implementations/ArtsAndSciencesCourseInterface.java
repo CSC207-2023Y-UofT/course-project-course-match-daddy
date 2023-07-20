302a2d6a54ec0c72dd0080a568620cba4e51da1f
realdevvora <91434020+realdevvora@users.noreply.github.com>
2023-07-20 19:15:13 -0400

    Update ArtsAndSciencesCourseInterface.java

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/interfaces/course_class_imports_implementations/ArtsAndSciencesCourseInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/interfaces/course_class_imports_implementations/ArtsAndSciencesCourseInterface.java
@@ -1,8 +1,4 @@
 package com.example.coursematchdaddy.clean_architecture_layers.entities.interfaces.course_class_imports_implementations;
 
-import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Course;
-import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
-
 public interface ArtsAndSciencesCourseInterface {
-    public boolean enroll(User user);
 }
