6c8f2d3ee2e1ea73e3bbb20950951ba280c6176c
realdevvora <91434020+realdevvora@users.noreply.github.com>
2023-07-19 21:31:12 -0400

    Update ExtractUserDataInterface.java

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/login_class_imports_implementations/ExtractUserDataInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/login_class_imports_implementations/ExtractUserDataInterface.java
@@ -1,8 +1,4 @@
 package com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.login_class_imports_implementations;
 
-import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Course;
-
-import java.util.HashMap;
-
 public interface ExtractUserDataInterface {
 }
