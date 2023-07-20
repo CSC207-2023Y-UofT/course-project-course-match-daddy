96d9198368ac6a1a15839776b0e612a41b656506
realdevvora <legitvoradev@gmail.com>
2023-07-20 08:51:07 -0400

    Add docstring

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/recommendationalgorithm_class_imports_implementations/ViewProgramDataInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/recommendationalgorithm_class_imports_implementations/ViewProgramDataInterface.java
@@ -3,5 +3,10 @@ package com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfa
 import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Program;
 
 public interface ViewProgramDataInterface {
+    /**
+     * return the program associated with the program code
+     * @param programCode
+     * @return Program
+     */
     public Program getProgramData(String programCode);
 }
