6049c397f5cc58a4a51205b3a3455df7fed1e439
uyoyo0 <96444432+uyoyo0@users.noreply.github.com>
2023-07-20 15:18:26 +0000

    This is a test

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/controllers/classes/LoginController.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/controllers/classes/LoginController.java
@@ -3,4 +3,5 @@ package com.example.coursematchdaddy.clean_architecture_layers.controllers.class
 import com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.login_class_imports_implementations.CollectLoginDataInterface;
 
 public class LoginController implements CollectLoginDataInterface {
+    //This is a better test!
 }
