49aa686541fcae12c91ade290a26ceb4ceac1448
realdevvora <91434020+realdevvora@users.noreply.github.com>
2023-07-20 19:46:49 -0400

    Update ExtractProgramsRecommendationsInterface.java

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/recommendationalgorithm_class_imports_implementations/ExtractProgramsRecommendationsInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/recommendationalgorithm_class_imports_implementations/ExtractProgramsRecommendationsInterface.java
@@ -1,10 +1,4 @@
 package com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.recommendationalgorithm_class_imports_implementations;
 
-import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Program;
-
 public interface ExtractProgramsRecommendationsInterface {
-    public String getTitle(Program programData);
-    public String getCode(Program programData);
-    public String getDescription(Program programData);
-    public String getImageURL(Program programData);
 }
