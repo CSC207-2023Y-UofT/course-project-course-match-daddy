55c5ad986c7fe6e15e0f67d1ffafb48520cdf14c
uyoyo0 <96444432+uyoyo0@users.noreply.github.com>
2023-07-20 09:10:25 -0400

    added comments to VerifyLoginDataInterface.java

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/login_class_imports_implementations/VerifyLoginDataInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/login_class_imports_implementations/VerifyLoginDataInterface.java
@@ -3,11 +3,34 @@ package com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfa
 import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
 import com.example.coursematchdaddy.clean_architecture_layers.gateways.classes.DBUsersGateway;
 
+/**
+ * The VerifyLoginDataInterface provides methods needed to verify the correctness of user login data.
+ */
 public interface VerifyLoginDataInterface {
 
-    final static DBUsersGateway gateway = new DBUsersGateway();
+    /**
+     * Verifies the correctness of the username provided in the user data.
+     *
+     * @param userData The User object containing the user data, including the username to be verified.
+     * @return true if the username is valid and matches an existing user account, false otherwise.
+     */
     boolean verifyUsername(User userData);
+
+    /**
+     * Verifies the correctness of the password provided in the user data.
+     *
+     * @param userData The User object containing the user data, including the password to be verified.
+     * @return true if the password is valid and matches the corresponding user's password, false otherwise.
+     */
     boolean verifyPassword(User userData);
+
+    /**
+     * Verifies the correctness of the email provided in the user data.
+     *
+     * @param userData The User object containing the user data, including the email to be verified.
+     * @return true if the email is valid and matches an existing user account, false otherwise.
+     */
     boolean verifyEmail(User userData);
 
 }
+
