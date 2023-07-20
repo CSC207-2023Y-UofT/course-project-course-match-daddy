5ca5f74d77f01a1cc43cc8121810c842f7f7a79c
realdevvora <91434020+realdevvora@users.noreply.github.com>
2023-07-20 18:53:42 -0400

    Update OpenEnrollmentCourseInterface.java

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/interfaces/course_class_imports_implementations/OpenEnrollmentCourseInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/interfaces/course_class_imports_implementations/OpenEnrollmentCourseInterface.java
@@ -1,13 +1,4 @@
 package com.example.coursematchdaddy.clean_architecture_layers.entities.interfaces.course_class_imports_implementations;
 
-import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
-
 public interface OpenEnrollmentCourseInterface {
-    public String getProgramArea();
-    public String getBreadth();
-    public int getRemainingSeats();
-    public String getDistribution();
-    public boolean enroll(User user);
-    public boolean unenroll(User user);
-
 }
