04c5df5df8aca62daf8554275716b7b0d6596eb7
realdevvora <91434020+realdevvora@users.noreply.github.com>
2023-07-20 19:18:24 -0400

    Update ClosedEnrollmentCourseInterface.java

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/interfaces/course_class_imports_implementations/ClosedEnrollmentCourseInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/interfaces/course_class_imports_implementations/ClosedEnrollmentCourseInterface.java
@@ -1,9 +1,4 @@
 package com.example.coursematchdaddy.clean_architecture_layers.entities.interfaces.course_class_imports_implementations;
 
-import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Course;
-import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
-
 public interface ClosedEnrollmentCourseInterface {
-    public boolean meetsPrerequisites(User user);
-    public boolean enroll(User user);
 }
