c8f010c45cf4df059a8afeaa48dfe1ed240f1a0a
uyoyo0 <96444432+uyoyo0@users.noreply.github.com>
2023-07-20 19:26:52 -0400

    Update ProgramRecommendationsPresenter.java

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/presenters/classes/ProgramRecommendationsPresenter.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/presenters/classes/ProgramRecommendationsPresenter.java
@@ -1,4 +1,5 @@
 package com.example.coursematchdaddy.clean_architecture_layers.presenters.classes;
+import com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.recommendationalgorithm_class_imports_implementations.ViewProgramsRecommendationsInterface;
 
 public class ProgramRecommendationsPresenter implements ViewProgramsRecommendationsInterface {
 }
