335a9010404315f62283f9ebf27829785a1ed3fc
realdevvora <legitvoradev@gmail.com>
2023-07-19 21:41:10 -0400

    Added method to interface

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/recommendationalgorithm_class_imports_implementations/ViewCourseDataInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/recommendationalgorithm_class_imports_implementations/ViewCourseDataInterface.java
@@ -1,4 +1,7 @@
 package com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.recommendationalgorithm_class_imports_implementations;
 
+import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Course;
+
 public interface ViewCourseDataInterface {
+    public Course getCourseData(String courseCode);
 }
