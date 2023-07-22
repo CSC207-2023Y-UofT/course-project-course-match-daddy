a4144ba9c2991963379c76cc8d8466502424946a
uyoyo0 <96444432+uyoyo0@users.noreply.github.com>
2023-07-22 04:33:49 +0000

    Added implementation of Login and its subclasses

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/classes/login_subclasses/CollectLoginData.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/classes/login_subclasses/CollectLoginData.java
@@ -1,6 +1,33 @@
+
 package com.example.coursematchdaddy.clean_architecture_layers.use_cases.classes.login_subclasses;
 
 import com.example.coursematchdaddy.clean_architecture_layers.use_cases.classes.Login;
+import com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.login_class_imports_implementations.CollectLoginDataInterface;
 
 public class CollectLoginData extends Login {
+
+    private CollectLoginDataInterface collectLoginData;
+
+    /**
+     * Creates a new CollectLoginData instance with the provided login credentials and data collection interface.
+     *
+     * @param username The username of the user whose login data needs to be collected and stored.
+     * @param password The password of the user whose login data needs to be collected and stored.
+     * @param email The email of the user whose login data needs to be collected and stored.
+     * @param collectLoginData The implementation of CollectLoginDataInterface to use for data collection and storage.
+     */
+    public CollectLoginData(String username, String password, String email, CollectLoginDataInterface collectLoginData) {
+        super(username, password, email);
+        this.collectLoginData = collectLoginData;
+    }
+
+    /**
+     * Collects user login data and stores it using the implementation of CollectLoginDataInterface.
+     * It stores the user's email, password, and username in the desired persistence.
+     */
+    public void storeData() {
+        this.collectLoginData.storeEmail(super.getEmail());
+        this.collectLoginData.storePassword(super.getPassword());
+        this.collectLoginData.storeUsername(super.getUsername());
+    }
 }
--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/classes/login_subclasses/VerifyLoginData.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/classes/login_subclasses/VerifyLoginData.java
@@ -1,6 +1,34 @@
+
 package com.example.coursematchdaddy.clean_architecture_layers.use_cases.classes.login_subclasses;
 
+import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
 import com.example.coursematchdaddy.clean_architecture_layers.use_cases.classes.Login;
+import com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.login_class_imports_implementations.VerifyLoginDataInterface;
 
 public class VerifyLoginData extends Login {
+
+    private VerifyLoginDataInterface verifyLoginData;
+
+    /**
+     * Creates a new VerifyLoginData instance with the specified login credentials and verification interface.
+     *
+     * @param username The username of the user trying to log in.
+     * @param password The password of the user trying to log in.
+     * @param email The email of the user trying to log in.
+     * @param verifyLoginData The implementation of VerifyLoginDataInterface to use for data verification.
+     */
+    public VerifyLoginData(String username, String password, String email, VerifyLoginDataInterface verifyLoginData) {
+        super(username, password, email);
+        this.verifyLoginData = verifyLoginData;
+    }
+
+    /**
+     * Verifies the user login data using the implementation of VerifyLoginDataInterface.
+     *
+     * @return true if the user login data is valid, false otherwise.
+     */
+    public boolean verifyData() {
+        User user = super.getUser();
+        return this.verifyLoginData.verifyEmail(user) && this.verifyLoginData.verifyUsername(user) && this.verifyLoginData.verifyPassword(user);
+    }
 }
