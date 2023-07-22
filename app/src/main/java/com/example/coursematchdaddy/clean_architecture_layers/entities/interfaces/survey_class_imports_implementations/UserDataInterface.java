8a538dc2863c37f47cd84cb28bdd9be3e0a73dff
GagandeepLubana <gagandeeplubana1221@gmail.com>
2023-07-22 11:35:17 -0400

    resolved conflicts

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/interfaces/survey_class_imports_implementations/UserDataInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/interfaces/survey_class_imports_implementations/UserDataInterface.java
@@ -1,6 +1,5 @@
 package com.example.coursematchdaddy.clean_architecture_layers.entities.interfaces.survey_class_imports_implementations;
 
-import java.util.ArrayList;
 
 public interface UserDataInterface {
 }
