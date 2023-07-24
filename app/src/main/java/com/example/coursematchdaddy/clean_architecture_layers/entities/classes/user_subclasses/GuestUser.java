cea93555d2f44428428d11d0c2ad5bbd5583954c
Jaspreet Khela <jaspreetkhela@Jaspreets-MBP.ht.home>
2023-07-24 06:30:10 -0400

    Defined GuestUserInterface (Entity-Layer) interface and GuestUser (Entity-Layer) subclass's constructor method

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/user_subclasses/GuestUser.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/user_subclasses/GuestUser.java
@@ -3,4 +3,14 @@ package com.example.coursematchdaddy.clean_architecture_layers.entities.classes.
 import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
 
 public class GuestUser extends User {
+    /**
+     * Constructor method for the User class.
+     *
+     * @param username This is the username of a user; this is a unique identifier for the user.
+     * @param email    This is the email of a user; this is a unique identifier for the user.
+     * @param password This is the password of a user.
+     */
+    public GuestUser() {
+        super("username", "email", "password");
+    }
 }
