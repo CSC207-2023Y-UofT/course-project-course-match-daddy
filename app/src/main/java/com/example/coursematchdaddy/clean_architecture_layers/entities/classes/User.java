49466617f8f16534cead7748d021458d9c0bc464
Jaspreet Khela <jaspreetkhela@Jaspreets-MBP.ht.home>
2023-07-24 07:29:10 -0400

    Defined the UpdateSettings (Use Case-Layer) class and updated the User (Entity-Layer) class

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/User.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/User.java
@@ -69,4 +69,79 @@ public abstract class User {
     public Survey getUserSurveyData(){
         return this.userSurvey;
     }
+
+    /**
+     * Update a user's email.
+     *
+     * @param email A user's email.
+     * @return Return true if update is successful.
+     */
+    public boolean updateUserEmail(String email){
+        // Update the private email variable.
+        this.email = email;
+
+        return true;
+    }
+
+    /**
+     * Update a user's password.
+     *
+     * @param password A user's password.
+     * @return Return true if update is successful.
+     */
+    public boolean updateUserPassword(String password){
+        // Update the private password variable.
+        this.password = password;
+
+        return true;
+    }
+
+    /**
+     * Update a user's selected courses.
+     *
+     * @param selectedCourses A user's selected courses.
+     * @return Return true if update is successful.
+     */
+    public boolean updateUserSelectedCourses(Map<String, Course> selectedCourses){
+        this.selectedCourses = selectedCourses;
+
+        return true;
+    }
+
+    /**
+     * Update a user's selected programs.
+     *
+     * @param selectedPrograms A user's selected programs.
+     * @return Return true if update is successful.
+     */
+    public boolean updateUserSelectedPrograms(Map<String, Program> selectedPrograms){
+        this.selectedPrograms = selectedPrograms;
+
+        return true;
+    }
+
+    /**
+     * Update a user's survey data.
+     *
+     * @param userSurvey A user's survey data.
+     * @return Return true if update is successful.
+     */
+    public boolean updateUserSurveyData(Survey userSurvey){
+        this.userSurvey = userSurvey;
+
+        return true;
+    }
+
+    /**
+     * Update a user's username.
+     *
+     * @param username A user's username.
+     * @return Return true if update is successful.
+     */
+    public boolean updateUserUsername(String username){
+        // Update the private email variable.
+        this.username = username;
+
+        return true;
+    }
 }
