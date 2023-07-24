0c6a2ce26eeae1ce53cef4ec998dd0500b9aa672
Jaspreet Khela <jaspreetkhela@Jaspreets-MBP.ht.home>
2023-07-24 14:08:24 -0400

    Defined CollectSettingsDataInterface (Use Case-Layer) interface

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/updatesettings_class_imports_implementations/CollectSettingsDataInterface.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/interfaces/updatesettings_class_imports_implementations/CollectSettingsDataInterface.java
@@ -1,4 +1,14 @@
 package com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.updatesettings_class_imports_implementations;
 
+import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
+
+// Provides the method(s) that collects a user’s provided settings data.
 public interface CollectSettingsDataInterface {
+    /**
+     * Retrieve a user's provided data from the settings view.
+     *
+     * @param userData This is an instance of a User class.
+     * @return Return true if the data was successfully collected.
+     */
+    public boolean collectSettingsData(User userData);
 }
