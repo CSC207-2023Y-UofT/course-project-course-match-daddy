82c676e89e132f4295d733a32e9a46269f01b21f
uyoyo0 <96444432+uyoyo0@users.noreply.github.com>
2023-07-26 10:34:15 -0400

    Update ViewCourseData.java
    <getCourseData> was implemented incorrectly (it assumed <getSelectedCourses> returned a list intsead of map) so I fixed it

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/classes/recommendationalgorithm_subclasses/ViewCourseData.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/classes/recommendationalgorithm_subclasses/ViewCourseData.java
@@ -1,19 +1,20 @@
 package com.example.coursematchdaddy.clean_architecture_layers.use_cases.classes.recommendationalgorithm_subclasses;
 
 import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Course;
+import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
 import com.example.coursematchdaddy.clean_architecture_layers.use_cases.classes.RecommendationAlgorithm;
 
 public class ViewCourseData extends RecommendationAlgorithm {
+    public ViewCourseData(User user) {
+        super(user);
+    }
+
     /**
      * return the course associated with courseCode
      * @param courseCode: String
      * @return Course
      */
     public Course getCourseData(String courseCode) {
-        for (Course c: this.user.getSelectedCourses()) {
-            if (c.getCourseCode() == courseCode) {
-                return c;
-            }
-        }
+        return super.getUser().getSelectedCourses().get(courseCode);
     }
 }
