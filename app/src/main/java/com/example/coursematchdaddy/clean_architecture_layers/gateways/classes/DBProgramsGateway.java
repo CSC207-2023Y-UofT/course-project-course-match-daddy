f27d09ed289f8cd76f8011f52143c80404f65b74
realdevvora <legitvoradev@gmail.com>
2023-07-27 19:53:33 -0400

    Updated programsgateway

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/gateways/classes/DBProgramsGateway.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/gateways/classes/DBProgramsGateway.java
@@ -4,18 +4,39 @@ import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.P
 import com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.swipecarousel_class_imports_implementations.ExtractProgramDataInterface;
 
 public class DBProgramsGateway implements ExtractProgramDataInterface {
+
+    /**
+     * return program title from the database
+     *
+     * @param programData : Program
+     * @return String
+     */
     @Override
     public String getTitle(Program programData) {
-        return null;
+        return programData.getProgramTitle();
     }
 
+    /**
+     * return program code from the database
+     *
+     * @param programData : Program
+     * @return String
+     */
     @Override
     public String getCode(Program programData) {
-        return null;
+        return programData.getProgramCode();
     }
 
+    /**
+     * return program description from the database
+     *
+     * @param programData : Program
+     * @return String
+     */
     @Override
     public String getDescription(Program programData) {
-        return null;
+        return programData.getProgramDescription();
     }
+
+
 }
