47ffc799af6890560b861c2bce32874b6364c2ba
lavya <lavyavaishno@gmail.com>
2023-07-27 15:53:26 -0400

    Completed the SwipeCarousel

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/classes/SwipeCarousel.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/classes/SwipeCarousel.java
@@ -1,4 +1,18 @@
 package com.example.coursematchdaddy.clean_architecture_layers.use_cases.classes;
 
-public class SwipeCarousel {
+import com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.swipecarousel_class_imports_implementations.CollectCarouselSwipeInterface;
+import com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.swipecarousel_class_imports_implementations.ExtractCourseDataInterface;
+import com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.swipecarousel_class_imports_implementations.ExtractProgramDataInterface;
+import com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.swipecarousel_class_imports_implementations.SwipeCardLeftInterface;
+import com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.swipecarousel_class_imports_implementations.SwipeCardRightInterface;
+
+public abstract class SwipeCarousel implements ExtractCourseDataInterface, ExtractProgramDataInterface, SwipeCardRightInterface,
+        SwipeCardLeftInterface, CollectCarouselSwipeInterface {
+    private void getCourseRecommendations() {
+    }
+    public abstract void extractCourseData(ExtractCourseDataInterface extractor);
+    public abstract void extractProgramData(ExtractProgramDataInterface extractor);
+    public abstract boolean addCoursePreference(Course courseData);
+    public abstract boolean removeCourseFromCarousel(SwipeCarousel carouselData, Course courseData);
+}
 }
