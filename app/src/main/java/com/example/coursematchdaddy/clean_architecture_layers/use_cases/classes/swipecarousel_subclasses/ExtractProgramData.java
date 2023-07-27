49da925bd2e4a20066fe1632b7eb678e0d0fca3a
uyoyo0 <96444432+uyoyo0@users.noreply.github.com>
2023-07-27 18:21:21 -0400

    Update ExtractProgramData.java
    temporary fix

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/classes/swipecarousel_subclasses/ExtractProgramData.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/classes/swipecarousel_subclasses/ExtractProgramData.java
@@ -1,7 +1,12 @@
 package com.example.coursematchdaddy.clean_architecture_layers.use_cases.classes.swipecarousel_subclasses;
 
+import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Course;
 import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Program;
 import com.example.coursematchdaddy.clean_architecture_layers.use_cases.classes.SwipeCarousel;
+import com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.swipecarousel_class_imports_implementations.ExtractCourseDataInterface;
+import com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.swipecarousel_class_imports_implementations.ExtractProgramDataInterface;
+
+import java.util.HashMap;
 
 public class ExtractProgramData extends SwipeCarousel {
     /**
@@ -31,4 +36,53 @@ public class ExtractProgramData extends SwipeCarousel {
         return programData.getProgramDescription();
     }
 
+    @Override
+    public void extractCourseData(ExtractCourseDataInterface extractor) {
+        
+    }
+
+    @Override
+    public void extractProgramData(ExtractProgramDataInterface extractor) {
+
+    }
+
+    @Override
+    public boolean addCoursePreference(Course courseData) {
+        return false;
+    }
+
+    @Override
+    public boolean removeCourseFromCarousel(SwipeCarousel carouselData, Course courseData) {
+        return false;
+    }
+
+    @Override
+    public String collectCarouselSwipe() {
+        return null;
+    }
+
+    @Override
+    public String getCourseTitle(Course courseData) {
+        return null;
+    }
+
+    @Override
+    public String getCourseCode(Course courseData) {
+        return null;
+    }
+
+    @Override
+    public String getCourseDescription(Course courseData) {
+        return null;
+    }
+
+    @Override
+    public String getImageURL(Course courseData) {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Object> getMiscellaneousCourseData(Course courseData) {
+        return null;
+    }
 }
