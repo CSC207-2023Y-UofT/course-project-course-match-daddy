89ca0afb2572cc7b59e993138c276a4f603d4e57
realdevvora <91434020+realdevvora@users.noreply.github.com>
2023-07-26 22:07:48 -0400

    Update SettingsPresenter.java

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/presenters/classes/SettingsPresenter.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/presenters/classes/SettingsPresenter.java
@@ -1,6 +1,19 @@
 package com.example.coursematchdaddy.clean_architecture_layers.presenters.classes;
 
+import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
 import com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.updatesettings_class_imports_implementations.ViewSettingsDataInterface;
 
+import java.util.HashMap;
+
 public class SettingsPresenter implements ViewSettingsDataInterface {
+    /**
+     * View a user's setting data.
+     *
+     * @param userData This is an instance of the User class.
+     * @return Return a HashMap that contains attributes of an instance of the User class.
+     */
+    @Override
+    public HashMap<String, Object> getUserData(User userData) {
+        return null;
+    }
 }
