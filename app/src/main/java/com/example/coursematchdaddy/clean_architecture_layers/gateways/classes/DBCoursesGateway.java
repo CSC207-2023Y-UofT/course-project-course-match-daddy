fc105d7c4676de0fa224483e3fe44030c9f7a3cd
uyoyo0 <96444432+uyoyo0@users.noreply.github.com>
2023-07-26 22:05:24 -0400

    Update DBCoursesGateway.java
    added dummy methods that havent been implemented

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/gateways/classes/DBCoursesGateway.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/gateways/classes/DBCoursesGateway.java
@@ -1,6 +1,33 @@
 package com.example.coursematchdaddy.clean_architecture_layers.gateways.classes;
 
+import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Course;
 import com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.swipecarousel_class_imports_implementations.ExtractCourseDataInterface;
 
+import java.util.HashMap;
+
 public class DBCoursesGateway implements ExtractCourseDataInterface {
+    @Override
+    public String getCourseTitle(Course courseData) {
+        return null;
+    }
+
+    @Override
+    public String getCourseCode(Course courseData) {
+        return null;
+    }
+
+    @Override
+    public String getCourseDescription(Course courseData) {
+        return null;
+    }
+
+    @Override
+    public String getImageURL(Course courseData) {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Object> getMiscellaneousCourseData(Course courseData) {
+        return null;
+    }
 }
