5d28fb92629030162ff90b87dc0ecd694977106f
realdevvora <91434020+realdevvora@users.noreply.github.com>
2023-07-27 13:19:48 -0400

    Update SettingsPresenter.java

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/presenters/classes/SettingsPresenter.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/presenters/classes/SettingsPresenter.java
@@ -1,19 +1,38 @@
 package com.example.coursematchdaddy.clean_architecture_layers.presenters.classes;
 
+import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.Survey;
 import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
 import com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.updatesettings_class_imports_implementations.ViewSettingsDataInterface;
 
 import java.util.HashMap;
 
+
 public class SettingsPresenter implements ViewSettingsDataInterface {
+
+    public SettingsPresenter() {
+    }
+
     /**
-     * View a user's setting data.
-     *
-     * @param userData This is an instance of the User class.
-     * @return Return a HashMap that contains attributes of an instance of the User class.
+     * Returns all of user data needed for settings
+     * @param userData
+     * @return HashMap<String, Object>
      */
-    @Override
     public HashMap<String, Object> getUserData(User userData) {
-        return null;
+
+        //Represents all of the user data as a hashmap
+        HashMap<String, Object> data = new HashMap<>();
+
+        data.put("email", userData.getEmail());
+
+        //Saves survey data of the user
+        Survey surveyData = userData.getUserSurveyData();
+
+        data.put("program", surveyData.getProgram());
+        data.put("numOfCredits", surveyData.getNumCredits());
+        data.put("courses", surveyData.getCoursesTaken());
+        data.put("preferences", surveyData.getPreferences());
+
+        return data;
+
     }
 }
