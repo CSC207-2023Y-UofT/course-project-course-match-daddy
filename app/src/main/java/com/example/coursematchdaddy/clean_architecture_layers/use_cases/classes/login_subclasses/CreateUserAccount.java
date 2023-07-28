abb859d3315263a0c5da31c21d2621330fb955f8
uyoyo0 <96444432+uyoyo0@users.noreply.github.com>
2023-07-28 11:01:18 -0400

    Update CreateUserAccount.java

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/classes/login_subclasses/CreateUserAccount.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/classes/login_subclasses/CreateUserAccount.java
@@ -1,16 +1,20 @@
+/**
+ * The CreateUserAccount class is responsible for creating user accounts and writing the account
+ * information to the database using the DBUsersGateway. It extends the DBUsersGateway class
+ * to access the necessary database functionality.
+ */
 package com.example.coursematchdaddy.clean_architecture_layers.use_cases.classes.login_subclasses;
 
-import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
 import com.example.coursematchdaddy.clean_architecture_layers.gateways.classes.DBUsersGateway;
-import com.example.coursematchdaddy.clean_architecture_layers.gateways.classes.GETCourseGateway;
-import com.example.coursematchdaddy.clean_architecture_layers.use_cases.classes.Login;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
 
 public class CreateUserAccount extends DBUsersGateway {
+
     /**
+     * Creates a new CreateUserAccount object with the specified username, password, and email.
      *
      * @param username The username of the user for the login.
      * @param password The password of the user for the login.
@@ -19,26 +23,35 @@ public class CreateUserAccount extends DBUsersGateway {
     private String username;
     private String email;
     private String password;
+
     public CreateUserAccount(String username, String password, String email) {
         this.username = username;
         this.password = password;
         this.email = email;
     }
 
-    public boolean createAccount(User userdata) {
-        try (FileWriter writer = new FileWriter(super.getuserDataPath(), true)) {
-            writer.append(this.username);
-            writer.append(",");
-            writer.append(this.email);
-            writer.append(",");
-            writer.append(this.password);
-            writer.append("\n");
-
+    /**
+     * Creates a new user account and writes the user information to the database.
+     *
+     * @return true if the account creation is successful, false otherwise.
+     */
+    public boolean createAccount() {
+        try {
+            // Get the file path for the user data from the DBUsersGateway
+            File file = new File(super.getuserDataPath());
+            try (FileWriter writer = new FileWriter(file, true)) {
+                // Write the user information to the file in CSV format
+                writer.append(this.username);
+                writer.append(",");
+                writer.append(this.email);
+                writer.append(",");
+                writer.append(this.password);
+                writer.append("\n");
+            }
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("Error occurred while writing to the CSV file.");
-            return false;//failed to create account
+            return false; // failed to create account
         }
-        return true;//Made account
+        return true; // Made account
     }
 }
