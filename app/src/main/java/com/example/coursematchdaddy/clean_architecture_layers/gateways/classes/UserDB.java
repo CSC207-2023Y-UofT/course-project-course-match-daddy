bbe192dfdbcf60a75df2654a23a1a4ac905d5fbd
uyoyo0 <96444432+uyoyo0@users.noreply.github.com>
2023-07-29 22:22:40 -0400

    Update UserDB.java
    Added exception handling and accounted for edge cases

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/gateways/classes/UserDB.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/gateways/classes/UserDB.java
@@ -1,110 +1,93 @@
 package com.example.coursematchdaddy.clean_architecture_layers.gateways.classes;
 
-import android.annotation.SuppressLint;
 import android.util.Log;
 
 import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
 import com.example.coursematchdaddy.clean_architecture_layers.use_cases.interfaces.login_class_imports_implementations.VerifyLoginDataInterface;
 
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Objects;
 
 public class UserDB implements VerifyLoginDataInterface {
-    @SuppressLint("SdCardPath")
     private final String pathname = "/data/user/0/com.example.coursematchdaddy/files/userdata.csv";
 
-    public UserDB() {}
+    public UserDB() {
+    }
+
     /**
-     * Read the database and put the information into a hashmap
+     * Read the user database from the file and put the information into a hashmap.
      *
-     * @return HashMap<String, User> : the hashmap containing all users
+     * @return HashMap<String, User>: the hashmap containing all users
      */
     private HashMap<String, User> readUserDB() {
         File file = new File(pathname);
 
-        try {
+        if (!file.exists()) {
+            return new HashMap<>();
+        }
 
-            // reading stream
-            FileInputStream fileInputStream = new FileInputStream(file);
-            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-            // converting to hashmap
+        try (FileInputStream fileInputStream = new FileInputStream(file);
+             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+
+            // Convert to hashmap
             Object userDB = objectInputStream.readObject();
-
-            objectInputStream.close();
-            fileInputStream.close();
-
             if (userDB instanceof HashMap) {
                 return (HashMap<String, User>) userDB;
             } else {
                 Log.e("ERROR", "Invalid data format in the user database file.");
             }
 
-            return new HashMap<>();
-
         } catch (ClassNotFoundException | IOException ex) {
-            throw new RuntimeException(ex);
+            Log.e("ERROR", "Error reading user database: " + ex.getMessage());
         }
+
+        return new HashMap<>();
     }
 
     /**
-     * Verify whether the user can be logged in based on the credentials given
+     * Verify whether the user can be logged in based on the credentials given.
      *
      * @param user the user with the information entered
-     * @return     whether or not the user can be logged in
+     * @return whether or not the user can be logged in
      */
     public boolean verifyUser(User user) {
         HashMap<String, User> userDB = readUserDB();
-        // iterating through hashmap to find user
 
-        // if there is a user in the database with the same username, deny the registration
+        // If there is a user in the database with the same username, deny the registration
         if (userDB.get(user.getUsername()) == null) {
             return writeUser(userDB, user);
-        }
-        else {
+        } else {
             return false;
         }
-
     }
 
     /**
-     * Write the new user information to the database
+     * Write the new user information to the database.
      *
      * @param userDB database
-     * @param user the user that is to be written
+     * @param user   the user that is to be written
      * @return boolean on whether the process was completed
      */
     private boolean writeUser(HashMap<String, User> userDB, User user) {
         File file = new File(pathname);
 
-        try {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(file);
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
 
-            // starting writers
-            FileOutputStream fileOutputStream = new FileOutputStream(file);
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-
-            // putting user and username into hashmap
+            // Put user and username into hashmap
             userDB.put(user.getUsername(), user);
 
-            // writing serialized object
+            // Write serialized object
             objectOutputStream.writeObject(userDB);
-
             Log.d("REGISTRATION SUCCESSFUL", "User: " + user.getUsername());
-
-            // closing writers
-            objectOutputStream.close();
-            fileOutputStream.close();
-
             return true;
+
         } catch (IOException ex) {
-            return false;
+            Log.e("ERROR", "Error writing user database: " + ex.getMessage());
         }
+
+        return false;
     }
 
     /**
@@ -116,18 +99,20 @@ public class UserDB implements VerifyLoginDataInterface {
      */
     @Override
     public boolean verifyUserProvidedData(String providedUsername, String providedPassword) {
-        // hashmap containing username : User key pair values
+        // Hashmap containing username : User key pair values
         HashMap<String, User> userDB = readUserDB();
+
         try {
-            // if username and password exist in DB, and are correct, then return true, otherwise, false
+            // If username and password exist in DB, and are correct, then return true, otherwise, false
             User user = userDB.get(providedUsername);
-            if (Objects.equals(user.getPassword(), providedPassword)) {
+            if (user != null && Objects.equals(user.getPassword(), providedPassword)) {
                 Log.d("LOGIN SUCCESSFUL", "User: " + user.getUsername());
                 return true;
             }
-        } catch (NullPointerException e)  {
-            return false;
+        } catch (Exception e) {
+            Log.e("ERROR", "Error verifying user login: " + e.getMessage());
         }
+
         return false;
     }
 }
