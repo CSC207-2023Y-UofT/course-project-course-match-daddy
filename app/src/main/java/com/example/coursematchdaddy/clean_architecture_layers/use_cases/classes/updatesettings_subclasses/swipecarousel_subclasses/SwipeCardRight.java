d03a40f39150b18fc836469fe53421e26249ac79
lavya <lavyavaishno@gmail.com>
2023-08-01 17:59:44 -0400

    Added Comments and Docstrings

--- app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/classes/updatesettings_subclasses/swipecarousel_subclasses/SwipeCardRight.java
+++ app/src/main/java/com/example/coursematchdaddy/clean_architecture_layers/use_cases/classes/updatesettings_subclasses/swipecarousel_subclasses/SwipeCardRight.java
@@ -14,12 +14,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-// The logic of this class is handled in the SwipeCarousel class
+/**
+ * This is a subclass of SwipeCarousel that represents the swipe right action.
+ * It extends the SwipeCarousel class.
+ */
 public class SwipeCardRight extends SwipeCarousel{
+
+    /**
+     * Constructor to initialize the SwipeCardRight.
+     *
+     * @param loggedInUser The logged-in user.
+     */
     public SwipeCardRight(User loggedInUser) {
         super(loggedInUser);
     }
 
+    /**
+     * This method represents the behavior for the right swipe action.
+     * Adds the course to the users selected courses
+     *
+     * @param courseData The course data for which the swipe action is performed.
+     * @return True to indicate that the course has been added to the selected list
+     */
     @Override
     public boolean processSwipe(Course courseData) {
         String courseString = courseData.getCourseTitle();
