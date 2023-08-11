ab6d2553cf8687fd055798f89b83fec961d3e919
realdevvora <legitvoradev@gmail.com>
2023-08-10 21:12:52 -0400

    Add remaining program tests

--- app/src/test/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/program_subclasses/Type2ProgramTest.java
+++ app/src/test/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/program_subclasses/Type2ProgramTest.java
@@ -1,12 +1,33 @@
 package com.example.coursematchdaddy.clean_architecture_layers.entities.classes.program_subclasses;
 
+import static org.junit.Assert.assertEquals;
+
+import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
+import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.user_subclasses.GuestUser;
+
 import junit.framework.TestCase;
 
 public class Type2ProgramTest extends TestCase {
 
     public void testEnroll() {
+        Type2Program program = new Type2Program("Religion", "ERMAJ0151", "Religion major", "RLG101");
+
+        assertEquals(program.getProgramCode(), "ERMAJ0151");
+        assertEquals(program.getProgramDescription(), "Religion major");
+        assertEquals(program.getProgramTitle(), "Religion");
+        assertEquals(program.getCompletionRequirements(), "RLG101");
+        assertEquals((int)program.getRelevance(), 0);
+
+        program.setRelevance(1);
+
+        assertEquals((int)program.getRelevance(), 1);
     }
 
     public void testUnenroll() {
+        Type2Program program = new Type2Program("Religion", "ERMAJ0151", "Religion major", "RLG101");
+        User temp = new GuestUser();
+        program.enroll(temp, true);
+
+        assertEquals(temp.getSelectedPrograms().get(program.getProgramTitle()), program);
     }
 }
--- app/src/test/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/program_subclasses/Type3ProgramTest.java
+++ app/src/test/java/com/example/coursematchdaddy/clean_architecture_layers/entities/classes/program_subclasses/Type3ProgramTest.java
@@ -1,15 +1,36 @@
 package com.example.coursematchdaddy.clean_architecture_layers.entities.classes.program_subclasses;
 
+import static org.junit.Assert.assertEquals;
+
+import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.User;
+import com.example.coursematchdaddy.clean_architecture_layers.entities.classes.user_subclasses.GuestUser;
+
 import junit.framework.TestCase;
 
 public class Type3ProgramTest extends TestCase {
+    Type3Program program = new Type3Program("Religion", "ERMAJ0151", "Religion major", "RLG101");
 
     public void testHasSpace() {
+        assertTrue(program.hasSpace());
     }
 
     public void testEnroll() {
+        assertEquals(program.getProgramCode(), "ERMAJ0151");
+        assertEquals(program.getProgramDescription(), "Religion major");
+        assertEquals(program.getProgramTitle(), "Religion");
+        assertEquals(program.getCompletionRequirements(), "RLG101");
+        assertEquals((int)program.getRelevance(), 0);
+
+        program.setRelevance(1);
+
+        assertEquals((int)program.getRelevance(), 1);
     }
 
     public void testUnenroll() {
+
+        User temp = new GuestUser();
+        program.enroll(temp, true);
+
+        assertEquals(temp.getSelectedPrograms().get(program.getProgramTitle()), program);
     }
 }
