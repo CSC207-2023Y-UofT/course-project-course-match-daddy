9751c996ee6fc3b33dad941afeadd6d4031a2779
uyoyo0 <96444432+uyoyo0@users.noreply.github.com>
2023-08-11 23:52:05 -0400

    Update LoginPresenterTest.java
    added tests

--- app/src/test/java/com/example/coursematchdaddy/clean_architecture_layers/presenters/classes/LoginPresenterTest.java
+++ app/src/test/java/com/example/coursematchdaddy/clean_architecture_layers/presenters/classes/LoginPresenterTest.java
@@ -5,8 +5,27 @@ import junit.framework.TestCase;
 public class LoginPresenterTest extends TestCase {
 
     public void testSetDisplayMessage() {
+        // Create an instance of LoginPresenter for testing
+        LoginPresenter loginPresenter = new LoginPresenter();
+
+        // Set a display message
+        loginPresenter.setDisplayMessage("Test message");
+
+        // Check if the stored display message matches the expected value
+        assertEquals("Test message", loginPresenter.getDisplayMessage());
     }
 
     public void testGetDisplayMessage() {
+        // Create an instance of LoginPresenter for testing
+        LoginPresenter loginPresenter = new LoginPresenter();
+
+        // Set a display message
+        loginPresenter.setDisplayMessage("Test message");
+
+        // Call the getDisplayMessage method
+        String displayMessage = loginPresenter.getDisplayMessage();
+
+        // Check if the returned display message matches the expected value
+        assertEquals("Test message", displayMessage);
     }
 }
